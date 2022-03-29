bd8055c0243c586eb4c0f62baa0ac444766f93ce
OLIVER EDDY <oliver.eddy@developersinstitute.co.nz>
2022-03-29 20:10:10 +1300

    added my custom Zetro Logo, My shop sells CD's something a bit old fasion to myself. I came up with the name Zetro for my store, Retro because because CD's are in fact retro and i added the Z for my generation the Zoomers or gen Z, We missed out on CD's so they are retro and cool to us, So Zetro is a combination of Retro and Generation Z, I feel like this embodys what a CD shop would be like today in the age of spodify and streaming, A little botuque store sell and the classic records, and a catchy name to go with it to captivade a younger audiance and hopyfully build some hype to get them intrested in the products

--- src/services/getProducts.js
+++ src/services/getProducts.js
@@ -2,13 +2,13 @@ const getProducts = async () => {
   // TODO Fetch products and prices from the Stripe API
   // Use the following URL for your fetch request
   const url = `${process.env.REACT_APP_API_URL}/products`;
-
+  const response = await fetch(url);
   const products = [];
-
+  const product = await response.json();
+  console.log(product);
   return products;
 };
 
 export { getProducts };
 
-//const response = await fetch(url);
-//const product = await response.json;
+//
