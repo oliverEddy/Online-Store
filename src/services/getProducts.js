72c19714eb6d921f7a3c9f089e04bde500f6d440
OLIVER EDDY <oliver.eddy@developersinstitute.co.nz>
2022-03-29 19:09:17 +1300

    started writing my getProducts function in getProducts.js, function fetches a data structure from my stripe api and pull in into my local machine so i can work with and display the values

--- src/services/getProducts.js
+++ src/services/getProducts.js
@@ -2,10 +2,11 @@ const getProducts = async () => {
   // TODO Fetch products and prices from the Stripe API
   // Use the following URL for your fetch request
   const url = `${process.env.REACT_APP_API_URL}/products`;
-
+  const response = await fetch(url);
   const products = [];
-
+  const item = await response.json();
   return products;
+  console.log(item);
 };
 
 export { getProducts };
